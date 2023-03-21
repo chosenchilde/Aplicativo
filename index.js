5165b348645b7a65776e12ba33f0d1c9828937f3
Chosen Child <edencyberspace@gmail.com>
2023-03-21 19:44:39 -0300

    HTML & Java

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+        Esse é o código JavaScript dessa página.
+        */
+
+        var name = 'Joca da Silva'
+        var email = 'joca@silva.com'
+        var birth = '2003-03-18'
+        var cpf = '012.345.678-90'
+        var address = 'Rua do Siri Molhado, 22'
+
+        // Dados do ususário. Forma moderna!
+        var user = {
+            name : 'Joca da Silva',
+            email : 'joca@silva.com',
+            birth : '2003-03-18',
+            cpf : '012.345.678-90',
+            address : 'Rua do Siri Molhado, 22'
+        }
