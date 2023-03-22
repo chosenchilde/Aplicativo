ec74c43e7560373bb918d1afa70eb464346b5ffa
Chosen Child <edencyberspace@gmail.com>
2023-03-21 21:46:49 -0300

    Inclusão de botão, CSS e botão funcional com JavaScript

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
         Esse é o código JavaScript dessa página.
         */
 
+        // Dados do usuário usando variáveis discretas.
         var name = 'Joca da Silva'
         var email = 'joca@silva.com'
         var birth = '2003-03-18'
@@ -10,15 +11,45 @@
 
         // Dados do ususário. Forma moderna!
         var user = {
+
+            //Atributos do objeto "user". 
             name : 'Joca da Silva',
             email : 'joca@silva.com',
             birth : '2003-03-18',
             cpf : '012.345.678-90',
             address : 'Rua do Siri Molhado, 22',
+            password:'12345',
+
+            // Métodos do objeto "user".
             login: function() {
-                prompt('Digite seu nome e e-mail')
+                
+                // Recebe e armazena o e-mail do prompt.
+                var userEmail = prompt('Digite seu e-mail')
+
+                // Se o usuário não digitou nada.
+                if(userEmail == "" || userEmail == null) {
+                    alert("Email não informado.")
+
+                    //Encerra a função retornando false.
+                    return false
+                } else if(userEmail != user.email) { 
+                    alert('E-mail não cadastrado.')
+                    return false 
+                }
+                    //Digite a senha.
+                    var userPassword = prompt('Digite a sua senha.')
+                    if (userPassword == "" || userPassword == null){
+                        alert("Você não informou a senha.")
+
+                        // Encerra a função retornando false.
+                        return false
+                    } else if(userPassword != user.password) {
+                    alert ('Senha não cadastrada')
+                    return false 
+                }
             },
             firstName: function(name) {
                 alert(name.split(' ')[0])
             }
         }
+
